638471720392d942fad66acd9b9c0b685743b9e2
cristianvsl <74607759+cristianvsl@users.noreply.github.com>
2020-12-15 10:57:56 +0000

    Update blocks.go

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/techievee/ethash-mining-pool/rpc"
-	"github.com/techievee/ethash-mining-pool/util"
+	"github.com/etclabscore/open-etc-pool/rpc"
+	"github.com/etclabscore/open-etc-pool/util"
 )
 
 const maxBacklog = 3
@@ -93,14 +93,6 @@ func (s *ProxyServer) fetchBlockTemplate() {
 	// Stratum
 	if s.config.Proxy.Stratum.Enabled {
 		go s.broadcastNewJobs()
-	}
-
-	if s.config.Proxy.StratumSSL.Enabled {
-		go s.broadcastSSLNewJobs()
-	}
-
-	if s.config.Proxy.StratumNiceHash.Enabled {
-		go s.broadcastNewJobsNH()
 	}
 }
 
